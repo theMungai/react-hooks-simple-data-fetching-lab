648cb4a7ea92040f2b12c38826923012f6c0f409
theMungai <mungaithesimon@gmail.com>
2025-04-28 12:20:48 +0300

    Data fetching

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,19 @@
 // create your App component here
+import React, {useState,useEffect, use} from 'react'
+
+function App(){
+    const [image, setImage] = useState(null)
+    useEffect(function(){
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then((res) => res.json())
+        .then((data) => setImage(data.message))
+    }, [])
+
+  return (
+    <div>
+      {image ? <img src={image} alt='A Random Dog'/> : <p>Loading...</p>}
+    </div>
+  )
+}
+
+export default App
